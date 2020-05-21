5cc3a482c89531585b01de80eaa6ea533f0977d3
Nicoly <nicolysantos51@gmail.com>
2020-05-21 18:03:35 -0300

    Redesign 🎨

--- js/js.js
+++ js/js.js
@@ -1,50 +1,31 @@
-setInterval(()=>{
-  let tempoSegundos = moment().format('ss');
-  let tempoMinutos = moment().format('mm');
-  let tempoHoras = moment().format('h');
+var horario = document.querySelector("#horario");
+var data = document.querySelector("#data");
 
-  tempoSegundos++;
+// Ponteiros
+var ponteiroSegundos = document.querySelector("#ponteiroSegundos");
+var ponteiroMinutos = document.querySelector("#ponteiroMinutos");
+var ponteiroHoras = document.querySelector("#ponteiroHoras");
 
-  if(tempoSegundos > 59){
-    tempoSegundos = 0;
-    tempoMinutos++;
-    if(tempoMinutos > 59){
-      tempoMinutos = 0;
-      tempoHoras++;
-      if(tempoHoras === 24){
-        tempoDia ++;
-         if(tempoDia > 31){
-           tempoDia = 1;
-           tempoMes++;
-            if(tempoMes > 12){
-              tempoMes = 1;
-              tempoAno++;
-            }
-         }
-        tempoHoras = 0;
-        tempoMinutos = 0;
-        tempoHoras = 0;
-      }
-    }
-  }
+setInterval(() => {
 
-  rodaPonteiros(tempoSegundos, tempoHoras, tempoMinutos);
-
-  let horarioEl = document.querySelector("#relogio");
-  horarioEl.innerHTML = tempoHoras + ":" + tempoMinutos + ":" + tempoSegundos;
-  horarioEl.innerHTML += " | " + moment().format("MMM Do YY");
+  atualizaNumeros(moment().format('h:mm:ss'), moment().format('d/MM/YY'));
+  atualizaPonteiros(moment().format('s'), moment().format('mm'), moment().format('h'));
 
 }, 1000);
 
-//---------------------------------------
-
-function rodaPonteiros(tempoSegundos, tempoHoras, tempoMinutos)
+function atualizaNumeros(tempoHorario, tempoData)
 {
-  let ponteiroSegundos = document.querySelector("#ponteiroSegundos");
-  let ponteiroMinutos = document.querySelector("#ponteiroMinutos");
-  let ponteiroHoras = document.querySelector("#ponteiroHoras");
+  
+  horario.textContent = moment().format('h:mm:ss');
+  data.textContent = moment().format('MMM do YY');
+
+}
+
+function atualizaPonteiros(tempoSegundos, tempoHoras, tempoMinutos)
+{
 
   ponteiroSegundos.style.transform = `rotate(${tempoSegundos * 6}deg)`;
   ponteiroHoras.style.transform = `rotate(${tempoHoras * 30}deg)`;
   ponteiroMinutos.style.transform = `rotate(${tempoMinutos * 6}deg)`;
+
 }
